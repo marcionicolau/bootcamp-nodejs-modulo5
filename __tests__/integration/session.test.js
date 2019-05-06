7c364442a9baa1fe9d9447e7f5872540c61ec96a
Marcio Nicolau <nicolau.3g@gmail.com>
2019-05-06 01:12:00 -0300

    Add user tests

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should be able to authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Marcio",
+      email: "mndev.ai@gmail.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Marcio",
+      email: "mndev.ai@gmail.com",
+      password_hash: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123456"
+      });
+
+    expect(response.status).toBe(401);
+  });
+});
