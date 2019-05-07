01abb9f8d810de517172edea5d9cbbad5aa3ca10
Marcio Nicolau <nicolau.3g@gmail.com>
2019-05-06 23:49:26 -0300

    finish tests

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 const { User } = require("../models");
 
+const Mail = require("../services/MailService");
+
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
@@ -13,6 +15,13 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: "Senha inválida" });
     }
+
+    await Mail.send({
+      from: "Marcio Nicolau <nicolau.3g@gmail.com>",
+      to: `${user.name} <${user.email}>`,
+      subject: "Novo acesso a sua conta",
+      text: "Olá"
+    });
 
     return res.json({
       token: await user.generateToken()
--- src/app/services/MailService.js
+++ src/app/services/MailService.js
@@ -2,5 +2,10 @@ const nodemailer = require("nodemailer");
 const mailConfig = require("../../config/mail");
 
 class MailService {
-  async send(message) {}
+  send(message) {
+    const transporter = nodemailer.createTransport(mailConfig);
+    return transporter.sendMail(message);
+  }
 }
+
+module.exports = new MailService();
